6ce6da74f3d6c5ae5a7fd7152ecf95b430528d3a
mlibai <mlibai@163.com>
2018-04-13 18:53:23 +0800

    Test

--- Example/Project/Example/Example/ViewController.swift
+++ Example/Project/Example/Example/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
+@IBDesignable open class OMLabel: UILabel {
+    
+    @IBInspectable open var contentInsets: UIEdgeInsets = UIEdgeInsets.init(top: 10, left: 10, bottom: 10, right: 10)
+    
+    override open func drawText(in rect: CGRect) {
+        let textRect = UIEdgeInsetsInsetRect(rect, contentInsets)
+        super.drawText(in: textRect)
+    }
+    
+    override open var intrinsicContentSize: CGSize {
+        let size1 = super.intrinsicContentSize
+        return CGSize.init(
+            width: size1.width + contentInsets.left + contentInsets.right,
+            height: size1.height + contentInsets.top + contentInsets.bottom
+        )
+    }
+    
+    override open func sizeThatFits(_ size: CGSize) -> CGSize {
+        let size1 = super.sizeThatFits(size)
+        return CGSize.init(
+            width: size1.width + contentInsets.left + contentInsets.right,
+            height: size1.height + contentInsets.top + contentInsets.bottom
+        )
+    }
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let label = OMLabel.init(frame: CGRect.init(x: 100, y: 100, width: 100, height: 100))
+        label.text = "This is a label."
+        label.backgroundColor = UIColor.lightGray
+        view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false;
+        let lc1 = NSLayoutConstraint(item: label, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+        let lc2 = NSLayoutConstraint(item: label, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0)
+        view.addConstraint(lc1)
+        view.addConstraint(lc2)
+        
     }
 
     override func didReceiveMemoryWarning() {
