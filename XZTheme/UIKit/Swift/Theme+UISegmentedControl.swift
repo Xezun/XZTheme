ce559df23171dd95a4da64efc893546866c534d2
mlibai <mlibai@163.com>
2018-05-28 00:16:18 +0800

    Theme for UISegmentedControl

---
+++ XZTheme/UIKit/Swift/Theme+UISegmentedControl.swift
@@ -0,0 +1,49 @@
+//
+//  Theme+UISegmentedControl.swift
+//  XZTheme
+//
+//  Created by mlibai on 2018/5/28.
+//
+
+import Foundation
+
+
+//extension Theme.Attribute {
+//    
+//    public static let <#Attribute Name#> = Theme.Attribute.init("<#T##rawValue: String##String#>")
+//    
+//}
+//
+//extension Theme.Style {
+//    
+//    public var <#string#>: String? {
+//        get { return stringValue(forThemeAttribute: .<#T##Theme.Attribute#>) }
+//        set { setValue(newValue, forThemeAttribute: .<#T##Theme.Attribute#>) }
+//    }
+//    
+//    public var <#color#>: UIColor? {
+//        get { return color(forThemeAttribute: .<#T##Theme.Attribute#>) }
+//        set { setValue(newValue, forThemeAttribute: .<#T##Theme.Attribute#>) }
+//    }
+//    
+//    public var <#image#>: UIImage? {
+//        get { return image(forThemeAttribute: .<#T##Theme.Attribute#>) }
+//        set { setValue(newValue, forThemeAttribute: .<#T##Theme.Attribute#>)}
+//    }
+//    
+//    public var <#bool#>: Bool {
+//        get { return boolValue(forThemeAttribute: .<#T##Theme.Attribute#>)  }
+//        set { setValue(newValue, forThemeAttribute: .<#T##Theme.Attribute#>) }
+//    }
+//    
+//}
+//
+//extension UISegmentedControl {
+//    
+//    open override func updateAppearance(with themeStyles: Theme.Style.Collection) {
+//        super.updateAppearance(with: themeStyles)
+//        
+//        
+//    }
+//    
+//}
