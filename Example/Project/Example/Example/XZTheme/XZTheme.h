f6594a2b37ad01201bd9aeac3871839225ba13ba
徐臻 <xuzhen@xuzhen.local>
2019-01-01 23:34:48 +0800

    优化

--- Example/Project/Example/Example/XZTheme/XZTheme.h
+++ Example/Project/Example/Example/XZTheme/XZTheme.h
@@ -6,28 +6,223 @@
 //  Copyright © 2018 mlibai. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "NSObject+XZThemeSupporting.h"
+#import <UIKit/UIKit.h>
 
-@class XZThemeStyleSheet;
+/// 主题状态。
+typedef NSString * XZThemeState NS_EXTENSIBLE_STRING_ENUM NS_SWIFT_NAME(XZThemeStyle.State);
+/// 主题属性。
+typedef NSString * XZThemeAttribute NS_EXTENSIBLE_STRING_ENUM NS_SWIFT_NAME(XZThemeStyle.Attribute);
+/// 主题标识符。
+typedef NSString * XZThemeIdentifier NS_EXTENSIBLE_STRING_ENUM NS_SWIFT_NAME(XZTheme.Identifier);
+
+FOUNDATION_EXPORT XZThemeState const XZThemeStateNone NS_SWIFT_NAME(none);
+
+@class XZTheme, XZThemeStyle, XZThemeStyleSheet;
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// 主题。
 NS_SWIFT_NAME(Theme)
 @interface XZTheme : NSObject <NSCopying>
 
-+ (XZTheme *)themeNamed:(NSString *)name NS_SWIFT_NAME(init(named:));
-
-@property (nonatomic, readonly) NSString *name;
-- (instancetype)init NS_UNAVAILABLE;
-
-- (BOOL)isEqualToTheme:(nullable XZTheme *)otherTheme;
-
+/// 默认主题。
 @property (class, nonatomic, readonly) XZTheme *defaultTheme NS_SWIFT_NAME(default);
+/// 当前主题。
 @property (class, nonatomic, readonly) XZTheme *currentTheme NS_SWIFT_NAME(current);
 
-- (nullable XZThemeStyleSheet *)themeStyleSheetForObject:(NSObject *)object NS_SWIFT_NAME(styleSheet(for:));
+/// 构造主题对象。
+///
+/// @param name 主题名称。
++ (XZTheme *)themeNamed:(NSString *)name NS_SWIFT_NAME(init(named:));
+- (instancetype)init NS_UNAVAILABLE;
 
+/// 主题名称。
+@property (nonatomic, readonly) NSString *name;
+
+/// 判断两个主题是否是同一主题。
+///
+/// @param otherTheme 待比较的主题。
+/// @return 是否相同。
+- (BOOL)isEqualToTheme:(nullable XZTheme *)otherTheme;
+
+/// 获取对象的主题样式表。
+///
+/// @param object 对象。
+/// @return 主题样式表对象。
+- (nullable XZThemeStyleSheet *)themeStyleSheetForObject:(NSObject *)object NS_SWIFT_NAME(themeStyleSheet(for:));
+
+@end
+
+
+/// 主题样式。
+NS_SWIFT_NAME(XZTheme.Style)
+@interface XZThemeStyle : NSObject
+
+/// 构造指定状态下的主题样式。
+///
+/// @param themeState 主题状态。
+/// @return 主题样式 XZThemeStyle 对象。
++ (XZThemeStyle *)themeStyleForState:(XZThemeState)themeState NS_SWIFT_NAME(init(for:));
+/// 给指定对象构造私有样式。
+///
+/// @param object 主题样式所属的对象。
+/// @return 主题样式 XZThemeStyle 对象。
++ (XZThemeStyle *)themeStyleForObject:(NSObject *)object NS_SWIFT_NAME(init(for:));
+/// 为对象构造指定状态下的主题样式。
+///
+/// @param themeState 主题状态。
+/// @param object 主题样式所属的对象。
+/// @return 主题样式 XZThemeStyle 对象。
++ (XZThemeStyle *)themeStyleForState:(XZThemeState)themeState object:(nullable NSObject *)object NS_SWIFT_NAME(init(for:object:));
+
+/// 主题状态。
+@property (nonatomic, copy, readonly) XZThemeState state;
+/// 主题属性与值。
+@property (nonatomic, readonly) NSDictionary<XZThemeAttribute, id> *attributedStyleValues;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// 获取主题属性对应的样式值。
+///
+/// @param themeAttribute 主题属性。
+/// @return 主题样式值。
+- (nullable id)valueForAttribute:(XZThemeAttribute)themeAttribute NS_SWIFT_NAME(value(for:));
+/// 设置主题属性样式值。
+///
+/// @param value 主题样式值。
+/// @param themeAttribute 主题属性。
+- (void)setValue:(nullable id)value forAttribute:(XZThemeAttribute)themeAttribute NS_SWIFT_NAME(setValue(_:for:));
+
+/// 获取主题属性对应的样式值。
+///
+/// @param themeAttribute 主题属性。
+/// @return 主题样式值。
+- (nullable id)objectForKeyedSubscript:(XZThemeAttribute)themeAttribute;
+/// 设置主题属性样式值。
+///
+/// @param value 主题样式值。
+/// @param themeAttribute 主题属性。
+- (void)setObject:(nullable id)value forKeyedSubscript:(XZThemeAttribute)themeAttribute;
+
+/// 主题样式中是否包含指定主题属性。
+///
+/// @param themeAttribute 主题属性。
+/// @return 是否包含主题属性。
+- (BOOL)containsAttribute:(XZThemeAttribute)themeAttribute NS_SWIFT_NAME(contains(_:));
+
+/// 将另一个主题样式中的所有值复制到当前主题样式中。
+///
+/// @param themeStyle 被复制到主题样式。
+- (void)addValuesFromThemeStyle:(nullable XZThemeStyle *)themeStyle NS_SWIFT_NAME(addValues(from:));
+
+/// 获取指定主题状态下的主题样式。
+///
+/// @param themeState 主题状态。
+/// @return 主题样式。
+- (nullable XZThemeStyle *)themeStyleIfLoadedForState:(XZThemeState)themeState NS_SWIFT_NAME(themeStyleIfLoaded(for:));
+/// 获取指定主题状态下的主题样式，懒加载。
+///
+/// @param themeState 主题状态。
+/// @return 主题样式。
+- (XZThemeStyle *)themeStyleForState:(XZThemeState)themeState NS_SWIFT_NAME(themeStyle(for:));
+
+- (nullable id)valueForAttribute:(XZThemeAttribute)themeAttribute forState:(XZThemeState)themeState NS_SWIFT_NAME(value(for:for:));
+- (void)setValue:(nullable id)value forAttribute:(XZThemeAttribute)themeAttribute forState:(XZThemeState)themeState NS_SWIFT_NAME(setValue(_:for:for:));
+
+@end
+
+
+/// 主题样式表。
+NS_SWIFT_NAME(XZTheme.StyleSheet)
+@interface XZThemeStyleSheet : NSObject
+/// xzss 文件路径。
+@property (nonatomic, readonly) NSURL *url;
+/// 样式。
+@property (nonatomic, readonly) NSDictionary<XZThemeIdentifier, XZThemeStyle *> *identifiedThemeStyles NS_SWIFT_NAME(identifiedStyles);
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithURL:(NSURL *)sheetURL NS_SWIFT_NAME(init(_:));
+
+- (nullable XZThemeStyle *)themeStyleForObject:(NSObject *)object NS_SWIFT_NAME(style(for:));
+
+- (void)addThemeStylesFromThemeStyleSheet:(nullable XZThemeStyleSheet *)otherStyleSheet NS_SWIFT_NAME(addStyles(from:));
+
+@end
+
+
+
+
+
+@interface NSObject (XZTheme)
+
+/// 主题标识符，用于在样式表中匹配主题样式。
+@property (nonatomic, nullable, setter=xz_setThemeIdentifier:) XZThemeIdentifier xz_themeIdentifier NS_SWIFT_NAME(themeIdentifier);
+/// 当前已应用的主题。
+@property (nonatomic, nullable, readonly) XZTheme *xz_appliedTheme NS_SWIFT_NAME(appliedTheme);
+/// 当前对象是否已经被标记为需要更新主题外观样式。
+@property (nonatomic, readonly) BOOL xz_needsUpdateThemeAppearance NS_SWIFT_NAME(needsUpdateThemeAppearance);
+
+/// 标记当前对象需要更新主题，在同一 runloop 中，该操作只会被标记一次，并在下一个 runloop 中执行更新操作。
+- (void)xz_setNeedsThemeAppearanceUpdate NS_SWIFT_NAME(setNeedsThemeAppearanceUpdate());
+/// 当前对象被标记为需要更新主题时，将主题事件传递给下一级或子级。
+- (void)xz_forwardThemeAppearanceUpdate NS_SWIFT_NAME(forwardThemeAppearanceUpdate());
+/// 如果当前对象已被标记需要更新主题，则立即执行更新主题的操作。
+- (void)xz_updateThemeAppearanceIfNeeded NS_SWIFT_NAME(updateThemeAppearanceIfNeeded());
+
+/// 应用新的主题，如果对象已被标记为需要更新主题，则此方法一定会被调用。默认情况下，此方法将查找当前对象的主题样式，并
+/// 调用应用主题样式的方法。
+- (void)xz_updateAppearanceWithTheme:(XZTheme *)newTheme;
+/// 应用主题样式。
+- (void)xz_updateAppearanceWithThemeStyle:(XZThemeStyle *)themeStyle;
+
+/// 当前类的全局主题样式，懒加载，不可被继承。
+///
+/// @param theme 主题。
+/// @return 全局主题样式。
++ (XZThemeStyle *)xz_themeStyleForTheme:(XZTheme *)theme NS_SWIFT_NAME(xz_themeStyle(for:));
+/// 当前类的全局主题样式，不可被继承。
+///
+/// @param theme 主题。
+/// @return 全局主题样式。
++ (XZThemeStyle *)xz_themeStyleIfLoadedForTheme:(XZTheme *)theme NS_SWIFT_NAME(xz_themeStyleIfLoaded(for:));
+
+/// 当前对象的私有样式，懒加载。
+///
+/// @param theme 主题。
+/// @return 私有样式。
+- (XZThemeStyle *)xz_themeStyleForTheme:(XZTheme *)theme NS_SWIFT_NAME(xz_themeStyle(for:));
+/// 当前对象的私有样式。
+///
+/// @param theme 主题。
+/// @return 私有样式。
+- (XZThemeStyle *)xz_themeStyleIfLoadedForTheme:(XZTheme *)theme NS_SWIFT_NAME(xz_themeStyleIfLoaded(for:));
+
+/// 计算样式，应用到当前对象上的样式，不可直接修改计算样式。
+@property (nonatomic, nullable, readonly) XZThemeStyle *xz_computedThemeStyle NS_SWIFT_NAME(computedThemeStyle);
+
+/// 样式表名，用于查找当前对象样式表样式，默认 nil ，没有样式表样式。
+@property (nonatomic, nullable, readonly) NSString *xz_themeStyleSheetName NS_SWIFT_NAME(themeStyleSheetName);
+
+@end
+
+@interface UIView (XZTheme)
++ (void)load;
+- (NSString *)xz_themeStyleSheetName;
+- (void)xz_forwardThemeAppearanceUpdate;
+@end
+
+@interface UIWindow (XZTheme)
+- (NSString *)xz_themeStyleSheetName;
+@end
+
+@interface UINavigationItem (XZTheme)
+- (NSString *)xz_themeStyleSheetName;
+- (void)xz_forwardThemeAppearanceUpdate;
+@end
+
+@interface UIViewController (XZTheme)
++ (void)load;
+- (NSString *)xz_themeStyleSheetName;
 @end
 
 NS_ASSUME_NONNULL_END
--- Example/Project/Example/Example/XZTheme/XZTheme.m
+++ Example/Project/Example/Example/XZTheme/XZTheme.m
@@ -7,14 +7,32 @@
 //
 
 #import "XZTheme.h"
-#import "XZThemeStyleSheet.h"
+#import <objc/runtime.h>
+
+XZThemeState const XZThemeStateNone = @"";
+
+static const void * const _themeIdentifier = &_themeIdentifier;
+static const void * const _appliedTheme = &_appliedTheme;
+static const void * const _needsUpdateThemeAppearance = &_needsUpdateThemeAppearance;
+static const void * const _computedThemeStyle = &_computedThemeStyle;
+static const void * const _themedStyles = &_themedStyles;
+
+@interface NSObject (XZThemeInternal)
+@property (nonatomic, nullable, strong, setter=xz_setComputedThemeStyle:) XZThemeStyle *xz_computedThemeStyle;
+@end
+
 
 @implementation XZTheme {
     NSMutableDictionary<NSString *, id> *_keyedStyleSheets;
 }
 
 + (XZTheme *)defaultTheme {
-    return nil;
+    static XZTheme *_defaultTheme = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _defaultTheme = [XZTheme themeNamed:@"default"];
+    });
+    return _defaultTheme;
 }
 
 + (XZTheme *)currentTheme {
@@ -97,6 +115,488 @@
         styleSheet = nil;
     }
     return styleSheet;
+}
+
+@end
+
+
+
+@interface XZThemeStyle ()
+- (instancetype)initWithState:(XZThemeState)themeState;
+@end
+@interface XZStatedThemeStyle : XZThemeStyle
+@property (nonatomic, readonly) NSMutableDictionary<XZThemeState, XZThemeStyle *> *statedThemeStyles;
+@end
+@interface XZObjectThemeStyle : XZStatedThemeStyle
+@property (nonatomic, weak, nullable, readonly) NSObject *object;
+- (instancetype)initWithObject:(NSObject *)object state:(XZThemeState)themeState;
+@end
+
+@implementation XZThemeStyle {
+    NSMutableDictionary *_attributedStyleValues;
+}
+
++ (XZThemeStyle *)themeStyleForState:(XZThemeState)themeState {
+    return [XZThemeStyle themeStyleForState:themeState object:nil];
+}
+
++ (XZThemeStyle *)themeStyleForObject:(NSObject *)object {
+    return [XZThemeStyle themeStyleForState:XZThemeStateNone object:object];
+}
+
++ (XZThemeStyle *)themeStyleForState:(XZThemeState)themeState object:(nullable NSObject *)object {
+    if ([themeState isEqualToString:XZThemeStateNone]) {
+        return [[XZStatedThemeStyle alloc] initWithState:themeState object:object];
+    } else {
+        return [[XZThemeStyle alloc] initWithState:themeState object:object];
+    }
+}
+
+- (instancetype)initWithState:(XZThemeState)themeState object:(id)object {
+    self = [super init];
+    if (self != nil) {
+        _object = object;
+        _state = [themeState copy];
+        _attributedStyleValues = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+- (NSDictionary<XZThemeAttribute,id> *)attributedStyleValues {
+    return _attributedStyleValues;
+}
+
+- (id)valueForAttribute:(XZThemeAttribute)themeAttribute {
+    return _attributedStyleValues[themeAttribute];
+}
+
+- (void)setValue:(id)value forAttribute:(XZThemeAttribute)themeAttribute {
+    _attributedStyleValues[themeAttribute] = value;
+    [_object xz_setComputedThemeStyle:nil];
+    [_object xz_setNeedsThemeAppearanceUpdate];
+}
+
+- (id)objectForKeyedSubscript:(XZThemeAttribute)themeAttribute {
+    return [self valueForAttribute:themeAttribute];
+}
+
+- (void)setObject:(id)value forKeyedSubscript:(XZThemeAttribute)themeAttribute {
+    [self setValue:value forAttribute:themeAttribute];
+}
+
+- (BOOL)containsAttribute:(XZThemeAttribute)themeAttribute {
+    return _attributedStyleValues[themeAttribute] != nil;
+}
+
+- (void)addValuesFromThemeStyle:(XZThemeStyle *)themeStyle {
+    if (themeStyle == nil) {
+        return;
+    }
+    [themeStyle.attributedStyleValues enumerateKeysAndObjectsUsingBlock:^(XZThemeAttribute _Nonnull themeAttribute, id _Nonnull value, BOOL * _Nonnull stop) {
+        [self->_attributedStyleValues setObject:value forKeyedSubscript:themeAttribute];
+    }];
+    // 如果目标样式是集合样式。
+    if ([themeStyle isKindOfClass:[XZStatedThemeStyle class]]) {
+        themeStyle = [(XZStatedThemeStyle *)themeStyle themeStyleIfLoadedForState:_state];
+        [themeStyle.attributedStyleValues enumerateKeysAndObjectsUsingBlock:^(XZThemeAttribute _Nonnull themeAttribute, id  _Nonnull value, BOOL * _Nonnull stop) {
+            [self->_attributedStyleValues setObject:value forKeyedSubscript:themeAttribute];
+        }];
+    }
+    [_object xz_setComputedThemeStyle:nil];
+    [_object xz_setNeedsThemeAppearanceUpdate];
+}
+
+- (XZThemeStyle *)themeStyleIfLoadedForState:(XZThemeState)themeState {
+    return self;
+}
+
+- (XZThemeStyle *)themeStyleForState:(XZThemeState)themeState {
+    return self;
+}
+
+- (id)valueForAttribute:(XZThemeAttribute)themeAttribute forState:(XZThemeState)themeState {
+    return [self valueForAttribute:themeAttribute forState:(XZThemeStateNone)];
+}
+
+- (void)setValue:(id)value forAttribute:(XZThemeAttribute)themeAttribute forState:(XZThemeState)themeState {
+    [self setValue:value forAttribute:themeAttribute forState:(XZThemeStateNone)];
+}
+
+@end
+
+
+@implementation XZStatedThemeStyle
+
+@synthesize statedThemeStyles = _statedThemeStyles;
+
+- (NSMutableDictionary<XZThemeState, XZThemeStyle *> *)statedThemeStyles {
+    if (_statedThemeStyles != nil) {
+        return _statedThemeStyles;
+    }
+    _statedThemeStyles = [NSMutableDictionary dictionary];
+    return _statedThemeStyles;
+}
+
+- (void)addValuesFromThemeStyle:(XZThemeStyle *)themeStyle {
+    if (themeStyle == nil) {
+        return;
+    }
+    if ([themeStyle isKindOfClass:[XZStatedThemeStyle class]]) {
+        [themeStyle.attributedStyleValues enumerateKeysAndObjectsUsingBlock:^(XZThemeAttribute  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+            [self setValue:obj forAttribute:key];
+        }];
+        XZStatedThemeStyle *statedThemeStyle = (XZStatedThemeStyle *)themeStyle;
+        [statedThemeStyle->_statedThemeStyles enumerateKeysAndObjectsUsingBlock:^(XZThemeState _Nonnull themeState, XZThemeStyle * _Nonnull themeStyle, BOOL * _Nonnull stop) {
+            XZThemeStyle *newThemeStyle = [self themeStyleForState:themeState];
+            [themeStyle.attributedStyleValues enumerateKeysAndObjectsUsingBlock:^(XZThemeAttribute _Nonnull themeAttribute, id  _Nonnull value, BOOL * _Nonnull stop) {
+                [newThemeStyle setValue:value forAttribute:themeAttribute];
+            }];
+        }];
+    } else {
+        XZThemeStyle *newThemeStyle = [self themeStyleForState:themeStyle.state];
+        [themeStyle.attributedStyleValues enumerateKeysAndObjectsUsingBlock:^(XZThemeAttribute _Nonnull themeAttribute, id  _Nonnull value, BOOL * _Nonnull stop) {
+            [newThemeStyle setValue:value forAttribute:themeAttribute];
+        }];
+    }
+    [self.object xz_setNeedsThemeAppearanceUpdate];
+}
+
+- (XZThemeStyle *)themeStyleIfLoadedForState:(XZThemeState)themeState {
+    if ([themeState isEqualToString:XZThemeStateNone]) {
+        return self;
+    }
+    return _statedThemeStyles[themeState];
+}
+
+- (XZThemeStyle *)themeStyleForState:(XZThemeState)themeState {
+    XZThemeStyle *themeStyle = [self themeStyleIfLoadedForState:themeState];
+    if (themeStyle != nil) {
+        return themeStyle;
+    }
+    themeStyle = [[XZThemeStyle alloc] initWithState:themeState object:self.object];
+    self.statedThemeStyles[themeState] = themeStyle;
+    return themeStyle;
+}
+
+- (id)valueForAttribute:(XZThemeAttribute)themeAttribute forState:(XZThemeState)themeState {
+    return [[self themeStyleIfLoadedForState:themeState] valueForAttribute:themeAttribute];
+}
+
+- (void)setValue:(id)value forAttribute:(XZThemeAttribute)themeAttribute forState:(XZThemeState)themeState {
+    [[self themeStyleForState:themeState] setValue:value forAttribute:themeAttribute];
+}
+
+@end
+
+
+
+
+@implementation XZThemeStyleSheet {
+    NSDictionary<XZThemeIdentifier, XZThemeStyle *> *_identifiedThemeStyles;
+}
+
+- (instancetype)initWithURL:(NSURL *)sheetURL {
+    self = [super init];
+    if (self != nil) {
+        _url = sheetURL;
+        _identifiedThemeStyles = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+- (NSDictionary<XZThemeIdentifier,XZThemeStyle *> *)identifiedThemeStyles {
+    return _identifiedThemeStyles;
+}
+
+- (XZThemeStyle *)themeStyleForObject:(NSObject *)object {
+    return nil;
+}
+
+- (void)addThemeStylesFromThemeStyleSheet:(XZThemeStyleSheet *)otherStyleSheet {
+    
+}
+
+@end
+
+
+
+
+
+
+@implementation NSObject (XZTheme)
+
+- (XZThemeIdentifier)xz_themeIdentifier {
+    return objc_getAssociatedObject(self, _themeIdentifier);
+}
+
+- (void)xz_setThemeIdentifier:(XZThemeIdentifier)xz_themeIdentifier {
+    objc_setAssociatedObject(self, _themeIdentifier, xz_themeIdentifier, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (XZTheme *)xz_appliedTheme {
+    return objc_getAssociatedObject(self, _appliedTheme);
+}
+
+- (BOOL)xz_needsUpdateThemeAppearance {
+    return objc_getAssociatedObject(self, _needsUpdateThemeAppearance);
+}
+
+- (void)xz_setNeedsThemeAppearanceUpdate {
+    if ([self xz_needsUpdateThemeAppearance]) {
+        return;
+    }
+    objc_setAssociatedObject(self, _needsUpdateThemeAppearance, @(YES), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    [self xz_forwardThemeAppearanceUpdate];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self xz_updateThemeAppearanceIfNeeded];
+    });
+}
+
+- (void)xz_forwardThemeAppearanceUpdate {
+    
+}
+
+- (void)xz_updateThemeAppearanceIfNeeded {
+    if ([self xz_needsUpdateThemeAppearance]) {
+        objc_setAssociatedObject(self, _needsUpdateThemeAppearance, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        XZTheme *newTheme = [XZTheme currentTheme];
+        if (![[self xz_appliedTheme] isEqualToTheme:newTheme]) {
+            // 主题发生改变，重置计算样式。
+            // TODO: 当主题样式发生改变时，也需要改变计算样式。
+            objc_setAssociatedObject(self, _computedThemeStyle, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        }
+        [self xz_updateAppearanceWithTheme:newTheme];
+        objc_setAssociatedObject(self, _appliedTheme, newTheme, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+}
+
+- (void)xz_updateAppearanceWithTheme:(XZTheme *)newTheme {
+    // TODO: - 计算样式根据对象类型与标识符进行缓存，以避免重复生成计算样式。
+    XZThemeStyle *computedThemeStyle = objc_getAssociatedObject(self, _computedThemeStyle);
+    if (computedThemeStyle == nil) {
+        // 1. XZSS样式
+        XZThemeStyle *themeStyle1 = [[newTheme themeStyleSheetForObject:self] themeStyleForObject:self];
+        // 2. 全局样式
+        XZThemeStyle *themeStyle2 = [self.class xz_themeStyleIfLoadedForTheme:newTheme];
+        // 3. 私有样式
+        XZThemeStyle *themeStyle3 = [self xz_themeStyleIfLoadedForTheme:newTheme];
+        // 4. 默认主题样式，xzss 样式在读取时，已包含默认主题样式。
+        XZThemeStyle *themeStyle4 = nil;
+        if (![newTheme isEqualToTheme:[XZTheme defaultTheme]]) {
+            XZThemeStyle *themeStyle1 = [self.class xz_themeStyleIfLoadedForTheme:[XZTheme defaultTheme]];
+            XZThemeStyle *themeStyle2 = [self xz_themeStyleIfLoadedForTheme:[XZTheme defaultTheme]];
+            if (themeStyle1 != nil || themeStyle2 != nil) {
+                themeStyle4 = [XZThemeStyle themeStyleForObject:self];
+                [themeStyle4 addValuesFromThemeStyle:themeStyle1];
+                [themeStyle4 addValuesFromThemeStyle:themeStyle2];
+            }
+        }
+        // 5. 计算样式
+        if (themeStyle1 != nil || themeStyle2 != nil || themeStyle3 != nil) {
+            if (themeStyle4 != nil) {
+                computedThemeStyle = themeStyle4;
+            } else {
+                computedThemeStyle = [XZThemeStyle themeStyleForObject:self];
+            }
+            [computedThemeStyle addValuesFromThemeStyle:themeStyle1];
+            [computedThemeStyle addValuesFromThemeStyle:themeStyle2];
+            [computedThemeStyle addValuesFromThemeStyle:themeStyle3];
+            objc_setAssociatedObject(self, _computedThemeStyle, computedThemeStyle, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+            [self xz_updateAppearanceWithThemeStyle:computedThemeStyle];
+        } else if (themeStyle4 != nil) {
+            objc_setAssociatedObject(self, _computedThemeStyle, themeStyle4, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+            [self xz_updateAppearanceWithThemeStyle:themeStyle4];
+        } else {
+            objc_setAssociatedObject(self, _computedThemeStyle, [NSNull null], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        }
+    } else if ([computedThemeStyle isKindOfClass:[XZThemeStyle class]]) {
+        [self xz_updateAppearanceWithThemeStyle:computedThemeStyle];
+    }
+}
+
+- (void)xz_updateAppearanceWithThemeStyle:(XZThemeStyle *)themeStyle {
+    
+}
+
+
+- (XZThemeStyle *)xz_computedThemeStyle {
+    XZThemeStyle *themeStyle = objc_getAssociatedObject(self, _computedThemeStyle);
+    if ([themeStyle isKindOfClass:[XZThemeStyle class]]) {
+        return themeStyle;
+    }
+    return nil;
+}
+
+
+- (XZThemeStyle *)xz_themeStyleForTheme:(XZTheme *)theme {
+    NSMutableDictionary<XZTheme *, XZThemeStyle *> *themedStyles = objc_getAssociatedObject(self, _themedStyles);
+    if (themedStyles == nil) {
+        themedStyles = [NSMutableDictionary dictionary];
+        objc_setAssociatedObject(self, _themedStyles, themedStyles, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    XZThemeStyle *themeStyle = themedStyles[theme];
+    if (themeStyle != nil) {
+        return themeStyle;
+    }
+    themeStyle = [XZThemeStyle themeStyleForObject:self];
+    themedStyles[theme] = themeStyle;
+    return themeStyle;
+}
+
+- (XZThemeStyle *)xz_themeStyleIfLoadedForTheme:(XZTheme *)theme {
+    NSDictionary<XZTheme *, XZThemeStyle *> *themedStyles = objc_getAssociatedObject(self, _themedStyles);
+    return themedStyles[theme];
+}
+
++ (XZThemeStyle *)xz_themeStyleForTheme:(XZTheme *)theme {
+    NSMutableDictionary<XZTheme *, XZThemeStyle *> *themedStyles = objc_getAssociatedObject(self, _themedStyles);
+    if (themedStyles == nil) {
+        themedStyles = [NSMutableDictionary dictionary];
+        objc_setAssociatedObject(self, _themedStyles, themedStyles, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    XZThemeStyle *themeStyle = themedStyles[theme];
+    themedStyles[theme] = themeStyle;
+    return themeStyle;
+}
+
++ (XZThemeStyle *)xz_themeStyleIfLoadedForTheme:(XZTheme *)theme {
+    NSDictionary<XZTheme *, XZThemeStyle *> *themedStyles = objc_getAssociatedObject(self, _themedStyles);
+    return themedStyles[theme];
+}
+
+- (NSString *)xz_themeStyleSheetName {
+    return nil;
+}
+
+@end
+
+
+
+static const void * const _themeStyleSheetName = &_themeStyleSheetName;
+
+@implementation UIView (XZTheme)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class cls = [UIView class];
+        Method m1 = class_getInstanceMethod(cls, @selector(willMoveToSuperview:));
+        Method m2 = class_getInstanceMethod(cls, @selector(XZTheme_willMoveToSuperview:));
+        method_exchangeImplementations(m1, m2);
+    });
+}
+
+- (void)XZTheme_willMoveToSuperview:(nullable UIView *)newSuperview {
+    [self XZTheme_willMoveToSuperview:newSuperview];
+    
+    // 不在父视图上的控件没有显示，不需要操作。
+    if (newSuperview == nil) { return; }
+    // 如果已应用的主题与当前主题一致，不需要操作。
+    // 如果视图没有配置过主题，但是不代表子视图没有配置主题。
+    if ([[self xz_appliedTheme] isEqual:[XZTheme currentTheme]]) {
+        return;
+    }
+    // MARK: 仅标记是否在显示效果上会延迟，待验证。
+    [self xz_setNeedsThemeAppearanceUpdate];
+}
+
+- (NSString *)xz_themeStyleSheetName {
+    return self.nextResponder.xz_themeStyleSheetName;
+}
+
+- (void)xz_forwardThemeAppearanceUpdate {
+    for (UIView *subview in self.subviews) {
+        [subview xz_setNeedsThemeAppearanceUpdate];
+    }
+}
+
+@end
+
+@implementation UIWindow (XZTheme)
+
+- (NSString *)xz_themeStyleSheetName {
+    // TODO: - 样式表默认名称待确认。
+    return [NSBundle.mainBundle.infoDictionary objectForKeyedSubscript:@"Theme"];
+}
+
+@end
+
+@implementation UINavigationItem (XZTheme)
+
+- (NSString *)xz_themeStyleSheetName {
+    return objc_getAssociatedObject(self, _themeStyleSheetName);
+}
+
+- (void)xz_forwardThemeAppearanceUpdate {
+    [self.backBarButtonItem xz_setNeedsThemeAppearanceUpdate];
+    
+    NSArray<UIBarButtonItem *> *leftBarButtonItems = self.leftBarButtonItems;
+    if (leftBarButtonItems.count > 0) {
+        for (UIBarButtonItem *barButtonItem in leftBarButtonItems) {
+            [barButtonItem xz_setNeedsThemeAppearanceUpdate];
+        }
+    }
+    
+    NSArray<UIBarButtonItem *> *rightBarButtonItems = self.rightBarButtonItems;
+    if (rightBarButtonItems.count > 0) {
+        for (UIBarButtonItem *barButtonItem in rightBarButtonItems) {
+            [barButtonItem xz_setNeedsThemeAppearanceUpdate];
+        }
+    }
+}
+
+@end
+
+@implementation UIViewController (XZTheme)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class aClass = [UIViewController class];
+        Method imp1 = class_getInstanceMethod(aClass, @selector(viewWillAppear:));
+        Method imp2 = class_getInstanceMethod(aClass, @selector(XZTheme_viewWillAppear:));
+        method_exchangeImplementations(imp1, imp2);
+    });
+}
+
+- (void)XZTheme_viewWillAppear:(BOOL)animated {
+    [self XZTheme_viewWillAppear:animated];
+    
+    // 在控制器将要显示的时候，更新已应用的主题。
+    // 这个时候，控制器还没有显示，如果这个时候，更改了主题，会触发一个异步再次更新主题。
+    if ([self.xz_appliedTheme isEqual:[XZTheme currentTheme]]) {
+        return;
+    }
+    [self xz_setNeedsThemeAppearanceUpdate];
+}
+
+- (NSString *)xz_themeStyleSheetName {
+    // TODO: 在 Swift 环境中，此方法的返回值需验证。
+    return NSStringFromClass(self.class);
+}
+
+- (void)xz_forwardThemeAppearanceUpdate {
+    NSString * const themeStyleSheetName = self.xz_themeStyleSheetName;
+    
+    if (self.navigationController != nil) {
+        [self.navigationItem xz_setNeedsThemeAppearanceUpdate];
+        objc_setAssociatedObject(self.navigationItem, _themeStyleSheetName, themeStyleSheetName, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    }
+    if (self.tabBarController != nil) {
+        [self.tabBarItem xz_setNeedsThemeAppearanceUpdate];
+        objc_setAssociatedObject(self.tabBarItem, _themeStyleSheetName, themeStyleSheetName, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    }
+    NSArray<__kindof UIBarButtonItem *> *toolBarItems = self.toolbarItems;
+    if (toolBarItems.count > 0) {
+        for (UIBarButtonItem *toolBarItem in toolBarItems) {
+            [toolBarItem xz_setNeedsThemeAppearanceUpdate];
+            objc_setAssociatedObject(toolBarItem, _themeStyleSheetName, themeStyleSheetName, OBJC_ASSOCIATION_COPY_NONATOMIC);
+        }
+    }
+    for (UIViewController *childVC in self.childViewControllers) {
+        [childVC xz_setNeedsThemeAppearanceUpdate];
+    }
+    [self.presentedViewController xz_setNeedsThemeAppearanceUpdate];
 }
 
 @end
